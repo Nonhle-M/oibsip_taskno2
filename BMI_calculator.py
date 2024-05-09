8f14296a2a94d0ebd3b80fe7e8c955771987b1e1
Nonhle-M <mdlalosenontle@gmail.com>
2024-05-09 17:56:07 +0200

    Add files via upload
    Task 2:
    Create a basic BMI calculator

---
+++ BMI_calculator.py
@@ -0,0 +1,14 @@
+Weight=float(input("Enter persons weight: "))
+Height=float(input("Enter persons Height: "))
+while Weight != 0.0 or Height != 0.0 :
+   BMI = Weight/Height ** 2
+   if BMI < 18.5 :
+       print('BMI= ',BMI ,'  therefore Underweight') 
+   elif BMI >= 18 and BMI < 25 :
+       print('BMI= ',BMI ,'   therefore Normal')
+   else : 
+       print('BMI= ', BMI, '  therefore Overweight') 
+   Weight=float(input("Enter persons weight: "))
+   Height=float(input("Enter persons weight: "))
+
+
